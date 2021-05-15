b48173f8ff11e94bf859013fe89f8783447054d3
Soham Roy <sohamroycfc@gmail.com>
2021-05-15 07:02:43 +0530

    try catch for database

---
+++ flutterfire.dart
@@ -0,0 +1,25 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<void> signin(String email,String password) async{}
+
+
+Future<bool> register(String email,String password) async{
+  try{
+    await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+    return true;
+  }on FirebaseAuthException catch (e) {
+  if (e.code == 'weak-password') {
+    print('password provided is weak');
+  } 
+  else if (e.code == 'email-already-in-use') 
+  {
+    print('Account already exists for that email');
+  }
+  return false;
+} catch(e){
+  print(e.toString());
+  return false;
+}
+
+}
