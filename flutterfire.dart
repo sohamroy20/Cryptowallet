861e7311f8708cb3869d02ff55169257f4703632
Soham Roy <sohamroycfc@gmail.com>
2021-06-06 19:00:03 +0530

    integrated firetstore

--- flutterfire.dart
+++ flutterfire.dart
@@ -1,7 +1,16 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future<void> signin(String email,String password) async{}
+Future<bool> signin(String email,String password) async{
+  try{
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: email,password:password);
+    return true;
+  }catch(e){
+    print(e);
+    return false;
+  }
+}
 
 
 Future<bool> register(String email,String password) async{
@@ -21,5 +30,26 @@ Future<bool> register(String email,String password) async{
   print(e.toString());
   return false;
 }
+
+}
+// ignore: missing_return
+Future<bool> addCoin(String id, String amount) async{
+  try{
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    var value = double.parse(amount);
+    DocumentReference documentReference = FirebaseFirestore.instance.collection('Users').doc(uid).collection('coin').doc(id);
+    FirebaseFirestore.instance.runTransaction((transaction) async{
+      DocumentSnapshot snapshot=await transaction.get(documentReference);
+      if (!snapshot.exists){
+        documentReference.set({'Amount':value});
+        return true;
+      }
+      double newAmount = snapshot.data()['Amount']+value;
+      transaction.update(documentReference,{'Amount':newAmount});
+      return true;
+    });
+  }catch(e){
+    return false;
+  }
 
 }
